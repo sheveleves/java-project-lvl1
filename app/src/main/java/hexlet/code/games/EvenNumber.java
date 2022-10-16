caeab4dea1246e419696790473f3732363d11f1e
sheveleves <shevelve@gmail.com>
2022-10-16 18:35:37 +0500

    add isEven method

--- app/src/main/java/hexlet/code/games/EvenNumber.java
+++ app/src/main/java/hexlet/code/games/EvenNumber.java
@@ -7,6 +7,11 @@ public class EvenNumber {
     public static final int MAX_RANDOM_EVEN = 100; //максимальное случайное число
     public static final String EVEN_QUESTION = "Answer 'yes' if the number is even, otherwise answer 'no'.";
 
+
+    private static boolean isEven(int number) {
+        return (number % 2 == 0);
+    }
+
     public static void even() {
         int oneNumber; //хранит сгенерированное число
         String[][] dataGame = new String[Engine.MAX_COUNT][2];
@@ -14,7 +19,7 @@ public class EvenNumber {
         for (int i = 0; i < Engine.MAX_COUNT; i++) {
             oneNumber = RandomUtils.randomNumber(MAX_RANDOM_EVEN);
             dataGame[i][Engine.QUESTION_DATA] = Integer.toString(oneNumber);
-            if (oneNumber % 2 == 0) {
+            if (isEven(oneNumber)) {
                 dataGame[i][Engine.ANSWER_DATA] = "yes";
             } else {
                 dataGame[i][Engine.ANSWER_DATA] = "no";
